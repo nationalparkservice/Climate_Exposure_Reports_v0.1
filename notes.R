f769fbde16111c18b0e41f9a574577ed426404a1
mjcarlso <mjcarlso@colostate.edu>
2022-10-18 13:49:28 -0600

    fix knitting formating

---
+++ notes.R
@@ -0,0 +1,39 @@
+#----------------- Notes for running CF rmd------------------------
+
+
+
+####### Add to/change in rendered document
+
+  # Full name of the park in the first line of the last paragraph of the intro
+
+  # Park specific numbers in first paragraph of Projections of future climate exposure
+  # Link for methodological details in same paragraph
+
+  # Park specific numbers in first paragraph of Annual average temperature and precipitation
+
+  # Figure 2 caption if not warm wet and hot dry
+
+  #
+
+  # Appendix table
+
+
+
+####### Knitting issues/changes
+
+  # Multiple columns in intro
+
+  # change size of figure 1
+
+  # color blue for first 3 words of third paragraph
+
+
+
+
+
+
+
+
+
+
+
